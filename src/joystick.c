717af2d285cedb791ef8552653a3fe3064576a51
Murad <muradkarammaev@gmail.com>
2019-01-21 11:42:25 +0300

    cleanup: shorten code

--- src/joystick.c
+++ src/joystick.c
@@ -6,50 +6,36 @@
 
 #include "buttons.h"
 
-char parse_button(struct js_event *e) {
+void parse_button(struct js_event *e) {
 	printf("[%d]:[", e->time);
 	switch (e->number) {
-		case A_BTN:
-			printf("A"); break;
-		case B_BTN:
-			printf("B"); break;
-		case X_BTN:
-			printf("X"); break;
-		case Y_BTN:
-			printf("Y"); break;
-		case L_BTN:
-			printf("L"); break;
-		case R_BTN:
-			printf("R"); break;
-		case BACK_BTN:
-			printf("BACK"); break;
-		case START_BTN:
-			printf("START"); break;
-		case XBOX_BTN:
-			printf("XBOX"); break;
+		case A_BTN: printf("A"); break;
+		case B_BTN: printf("B"); break;
+		case X_BTN: printf("X"); break;
+		case Y_BTN: printf("Y"); break;
+		case L_BTN: printf("L"); break;
+		case R_BTN: printf("R"); break;
+		case BACK_BTN: printf("BACK"); break;
+		case START_BTN: printf("START"); break;
+		case XBOX_BTN: printf("XBOX"); break;
 	}
 	printf("] %s.\n", (e->value == 0x01) ? "pressed" : "released");
-
-	return EXIT_SUCCESS;
 }
 
-char parse_stick(struct js_event *e) {
+void parse_stick(struct js_event *e) {
 	printf("[%d]:[", e->time);
 	switch (e->number) {
-		case DPAD_X:
-			printf("DPAD X"); break;
-		case DPAD_Y:
-			printf("DPAD Y"); break;
+		case DPAD_X: printf("DPAD X"); break;
+		case DPAD_Y: printf("DPAD Y"); break;
 	}
 	printf("] %d.\n", e->value);
-	return EXIT_SUCCESS;
 }
 
-char log_event(struct js_event *e) {
+void log_event(struct js_event *e) {
 	printf("[%d]: input 0x%08x type of 0x%02x, button: 0x%02x.\n",
 			e->time, e->value, e->type, e->number);
-	return EXIT_SUCCESS;
 }
+
 
 int main(int argc, char *argv[]) {
 	char* jsfile;
@@ -68,12 +54,9 @@ int main(int argc, char *argv[]) {
 
 	while (read(fd, &e, sizeof(e)) == sizeof(e)) {
 		switch (e.type) {
-			case JS_EVENT_BUTTON:
-				parse_button(&e); break;
-			case JS_EVENT_AXIS:
-				parse_stick(&e); break;
-			default:
-				log_event(&e); break;
+			case JS_EVENT_BUTTON: parse_button(&e); break;
+			case JS_EVENT_AXIS: parse_stick(&e); break;
+			default: log_event(&e); break;
 		}
 	}
 
