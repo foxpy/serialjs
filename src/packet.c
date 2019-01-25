f9d59b9dab6b43dde7f2994a1ebd6a1b47c8c5e5
Murad <muradkarammaev@gmail.com>
2019-01-25 12:54:37 +0300

    implement CRC32

--- src/packet.c
+++ src/packet.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+
+void crc32(uint8_t *dst, uint8_t *data, uint32_t len) {
+	uint32_t val;
+	uint32_t crc = 0xFFFFFFFF;
+
+	while(len--) {
+		val = (crc ^ *(data++)) & 0xFF;
+		for (char i = 0; i < 8; i++) {
+			val = (val & 1) ? (val>>1) ^ 0xEDB88320 : val>>1;
+		}
+		crc = val ^ crc>>8;
+	}
+	crc ^= 0xFFFFFFFF;
+
+	memcpy(dst, &crc, sizeof(crc));
+}
 
 void build_packet(char command, char *args, char *packet) {
 	packet[0] = 0xFF; // packet start identification
@@ -11,11 +28,8 @@ void build_packet(char command, char *args, char *packet) {
 
 	// packet[10] is reserved
 
-	// checksum ??? TODO: Write CRC32
-	// (https://en.wikipedia.org/wiki/Cyclic_redundancy_check)
-	for (int i = 11; i <= 14; i++) {
-		packet[i] = 0x00;
-	}
+	// checksum
+	crc32((uint8_t*) packet+11, (uint8_t*) packet, 10);
 
 	packet[15] = 0x00; // packet end identification
 }
