ce07c31b0b98cd6b2cf03aafd47f3e4629ff1454
Murad <muradkarammaev@gmail.com>
2019-02-01 14:44:58 +0300

    add command functions
    move_cmd() - apply speed by 2 4-byte floats
    action_cmd() - lift platform up or down
    toggle_cmd() - turn vacuum generator on or off
    
    Currently these functions are useless and what they do is just
    printing out packets meant to be sent over COM port.

---
+++ src/command.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "packet.h"
+
+#define PACKET_SIZE 16
+#define MOVE_CMD 0x01
+#define ACTION_CMD 0x11
+#define TOGGLE_CMD 0x12
+
+void show_packet(uint8_t *packet) {
+	printf("0x");
+	for (uint8_t i = 0; i < PACKET_SIZE; i++) {
+		printf("%02hhx", *(packet+i));
+	}
+	printf("\n");
+}
+
+void move_cmd(float x, float y) {
+	uint8_t packet[PACKET_SIZE];
+	float args[2] = {x, y};
+	build_packet(MOVE_CMD, (uint8_t*) &args, (uint8_t*) &packet);
+	show_packet((uint8_t*) &packet);
+}
+
+void action_cmd(int8_t s) {
+	uint8_t packet[PACKET_SIZE];
+	int8_t args[8] = {s, 0, 0, 0, 0, 0, 0, 0};
+	build_packet(ACTION_CMD, (uint8_t*) args, (uint8_t*) &packet);
+	show_packet((uint8_t*) &packet);
+}
+
+void toggle_cmd(uint8_t s) {
+	uint8_t packet[PACKET_SIZE];
+	uint8_t args[8] = {s, 0, 0, 0, 0, 0, 0, 0};
+	build_packet(TOGGLE_CMD, (uint8_t*) &args, (uint8_t*) &packet);
+	show_packet((uint8_t*) &packet);
+}
---
+++ src/command.h
@@ -0,0 +1,3 @@
+void move_cmd(float x, float y);
+void action_cmd(int8_t s);
+void toggle_cmd(uint8_t s);
