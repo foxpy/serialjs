bd5b4f5d48a620685dffcb7f1217f6466511b245
Murad <muradkarammaev@gmail.com>
2019-03-01 08:44:52 +0300

    fix wrong function declarations in headers
    It actually does not change anything, this is just me being scrupulous.

--- src/joystick.h
+++ src/joystick.h
@@ -1,4 +1,4 @@
 void process_button(struct js_event *e, int fd);
-void process_dpad(struct js_event *e, int fd);
+void process_dpad(struct js_event *e);
 void process_stick(struct js_event *e, int fd);
 void process_event(struct js_event *e, int fd);
--- src/packet.h
+++ src/packet.h
@@ -1,2 +1,2 @@
-void crc32(uint32_t *dst, void *data, size_t len);
+void crc32(uint32_t *dst, void *src, size_t len);
 void build_packet(uint8_t command, uint8_t *args, uint8_t *packet);
